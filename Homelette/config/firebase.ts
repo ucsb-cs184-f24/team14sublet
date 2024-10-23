2b12cce72b8a2135dec7a13b9e4d15bc35b433ea
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-10-23 13:14:09 -0700

    getAnalytics was breaking the app

--- Homelette/config/firebase.ts
+++ Homelette/config/firebase.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCJ_DV2YTIJm_oA-ITdy1I4kQAHRaB5fsk",
   authDomain: "team14-sublet-1034a.firebaseapp.com",
@@ -16,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-NN10YB68DC",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
